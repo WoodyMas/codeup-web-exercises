dba5fcd20a38d18fe1a386d2f7355eff0a635e58
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-17 18:31:02 -0500

    for loop pyramid complete

---
+++ js/for_loops.js
@@ -0,0 +1,94 @@
+// function showMultiplicationTable(num){
+//     for (let i = 1; i <=10; i++){
+//         console.log(`${num} x ${i} = ${i * num}`);
+//     }
+// }
+//
+// function isEvenOrOdd(num) {
+//     if (num %2 === 0) {
+//         return `${num} is even.`;
+//     } else {
+//         return `${num} is odd.`;
+//     }
+// }
+
+
+    // // This is how you get a random number between 50 and 100
+    // let allCones = Math.floor(Math.random() * 21) + 180;
+
+
+// for (let i = 1; i <= 10; i++) {
+//     let randomNum = Math.floor(Math.random() * 181) + 20;
+//     console.log(`${isEvenOrOdd(randomNum)}`);
+// }
+
+
+
+
+// Create a for loop to display the following
+//
+// for (let i = 100; i > 0; i-= 5){
+//     console.log(i);
+// }
+
+            /*
+            100
+            95
+            90
+            85
+            80
+            75
+            70
+            65
+            60
+            55
+            50
+            45
+            40
+            35
+            30
+            25
+            20
+            15
+            10
+            5
+            */
+
+// Create a for loop that uses console.log to create the output shown below.
+
+
+// for (let i = 1; i <= 9; i++) {
+//
+// }
+
+// for (i = 1; i <= 9; i++) {
+//     let res = [];
+//     for (a = 1; a <= i; a++) {
+//         res.push(i);
+//     }
+//     console.log(res.join(""));
+// }
+
+
+
+let n = 9; // height of pattern
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+    // Internal loop
+    for (let j = 1; j <= i; j++) {
+        string += i;
+    }
+    string += "\n";
+}
+console.log(string);
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
