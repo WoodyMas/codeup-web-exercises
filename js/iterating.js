022302289f0188ad41f0a9f21807aa3163c51f22
Mason Wudtee <mason.wudtee@gmail.com>
2022-08-19 10:50:17 -0500

    iterating array exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,44 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    let names = ["Danny", "David", "Mason", "Frances"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(`the names array has ${names.length} elements to it.`);
+    console.log(names);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    console.log(` `);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (let i = 0; i < names.length ; i++) {
+        console.log(names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
 
+    console.log(` `)
+    console.log(`for`)
+    names.forEach(function (element, index, array){
+        console.log(`element ${index} has the name ${element}`);
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +57,25 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    console.log(` `)
+    console.log(` `)
+    console.log(`Creating three functions, each accepting an array and returning an element from it`);
+    console.log(` `)
+    console.log(` `)
+
+    names.forEach((name, index) => {
+        console.log(`The ${index} name is ${name}`);
+    });
+
+    function first (array) {
+        return array[0];
+    }
+    function second (array) {
+        return array[1];
+    }
+
+    function last (array) {
+        return array[array.length-1];
+    }
 
 })();
