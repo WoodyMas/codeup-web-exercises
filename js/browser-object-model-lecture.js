34deb8af5e922036b37320cb3b2aa3fda95062c3
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-14 13:59:33 -0500

    adding browser-object-model lecture files

---
+++ js/browser-object-model-lecture.js
@@ -0,0 +1,14 @@
+// let sayHey = function () {
+//     let count = 0;
+//     function hey() {
+//         if (count < 10) {
+//             console.log("hey " + count);
+//             count++;
+//         } else {
+//             clearInterval();
+//         }
+//
+//     }
+//
+//     setInterval(hey, 1000); //says hey every 1000 milliseconds
+// }
