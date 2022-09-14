88f17765a7e42f57436647b221dd8540082a452b
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-14 14:23:21 -0500

    defuse-the-bom v2 file added

--- js/browser-object-model-lecture.js
+++ js/browser-object-model-lecture.js
@@ -12,3 +12,21 @@
 //
 //     setInterval(hey, 1000); //says hey every 1000 milliseconds
 // }
+
+function tenHeys () {
+    let count =1;
+    let thisWillStop = setInterval(function() {
+        if (count < 10) {
+            console.log("Hey " + count);
+            count++;
+        } else {
+            clearInterval(thisWillStop);
+        }
+    }, 1000);
+}
+
+setTimeout(wakeUpUser, 5000); // setTimeout(function, how many seconds it takes before it executes the function)
+
+function wakeUpUser() {
+    alert("WAKE UP!!!!!!!!!!");
+}
