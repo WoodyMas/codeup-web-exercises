5b9e54ae71d2e9313e2a90c4229897c06541a254
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-14 16:18:32 -0500

    adding document-object-model-lecture.css file

--- js/document-object-model-lecture.js
+++ js/document-object-model-lecture.js
@@ -1,3 +1,17 @@
 let accessTheDOM = document.getElementById("dc");
 let showMe = accessTheDOM.innerHTML;
 alert(showMe);
+
+let marvel = document.getElementById("marvel");
+
+let preference = "dc";
+
+let like = document.getElementById("like");
+let dontLike = document.getElementById("dontLike");
+let output = `${dontLike.innerHTML} ${document.getElementById(preference).innerHTML} movies`;
+console.log(output);
+
+// accessTheDOM.innerHTML = accessTheDOM.innerHTML + ' Animated Universe';
+
+accessTheDOM.innerHTML += marvel.innerHTML;
+
