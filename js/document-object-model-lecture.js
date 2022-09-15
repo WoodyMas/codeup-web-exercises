32d3a432c3b60259e2635044f9928e2f1dff143f
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-15 10:37:39 -0500

    document-object-model-lecture completion

--- js/document-object-model-lecture.js
+++ js/document-object-model-lecture.js
@@ -31,11 +31,37 @@
 //     console.log(index);
 // })
 
-let submitButton = document.getElementById('colorSubmission');
-submitButton.onclick = function (){
-    let colorInput = document.getElementById("colorPreference");
-    let paragraphs = document.getElementsByTagName("p");
-    for (let i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].style.color = colorInput.value;
+// let submitButton = document.getElementById('colorSubmission');
+// submitButton.onclick = function (){
+//     let colorInput = document.getElementById("colorPreference");
+//     let paragraphs = document.getElementsByTagName("p");
+//     for (let i = 0; i < paragraphs.length; i++) {
+//         paragraphs[i].style.color = colorInput.value;
+//     }
+// }
+
+// <p id="hulk" data-state="hulk" data-strength="super" data-intelligence="low">The Hulk</p>
+
+let theHulk = document.getElementById("hulk");
+// let hulkState = theHulk.getAttribute("data-state");
+let hulkState = theHulk.dataset.state;
+let hulkStrength = theHulk.dataset.strength;
+let hulkIntelligence = theHulk.dataset.intelligence;
+let hulkSummary = `The hulk is in ${hulkState} state. His strength is ${hulkStrength} and his intelligence is ${hulkIntelligence}.`
+// theHulk.innerText = hulkSummary;
+
+let transformButton = document.getElementById("transformHulk");
+transformButton.onclick = function () {
+    // alert("Hulk smash")
+    let theHulk = document.getElementById("hulk");
+    let hulkState = theHulk.dataset.state;
+    let hulkStrength = theHulk.dataset.strength;
+    let hulkIntelligence = theHulk.dataset.intelligence;
+    if (hulkState === "hulk") {
+        theHulk.dataset.state = "Bruce Banner";
+    } else {
+        theHulk.dataset.state = "hulk";
     }
+    theHulk.innerText = `The Hulk is in ${hulkState} state.`
 }
+
