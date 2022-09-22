1705ed3576ea2d23cfe2cfeb9749db98ce1e7871
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-22 13:35:28 -0500

    jquery_faq traversing progress (exercise)

---
+++ js/jquery_faq.js
@@ -0,0 +1,40 @@
+// ###################################### jQuery FAQ #############################################################
+
+//     Create some CSS rules that sets elements with the invisible class to visibility: hidden;.
+// Update the page with jQuery to add a link that toggles the class invisible on and off for all dd elements.
+$('#showHide').click(function (){
+    $('dd').toggleClass('invisible');
+
+});
+
+//     Bonus
+
+
+// When any of the dt elements is clicked, the element that was clicked should be highlighted.
+$('dt').click(function (){
+    $(this).css({
+        'background-color': 'lightblue'
+    })
+});
+
+// ###################################### jQuery Traversal Exercise ###############################################
+
+//     Under the FAQ, add 3 unordered lists like above. Each list should contain a national park name in an h3 element,
+//     and a ul of 4 facts about the park.
+
+
+//     Create a button that, when clicked, makes the last li in each ul have a yellow background.
+//
+//     When any h3 is clicked, the lis underneath it should be bolded. Use font-weight: bold to achieve this.
+//
+//     When any list item is clicked, first li of the parent ul should have a font color of blue.
+//
+//     Bonus
+// Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
+//
+//     The rules are the following:
+//
+//     The left frame swaps to the right and takes the image from the frame in the center.
+//     The center frame swaps randomly to either the left or right.
+//     The right frame swaps to the left and takes the image from the frame in the center.
+
