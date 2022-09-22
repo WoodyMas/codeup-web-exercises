3a374c75f0c2bd8103accd1d5f0e25301dbe9834
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-22 18:24:07 -0500

    jquery faqs, intro to ajax, ajax store files added and completed

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,3 +1,4 @@
+
 // ###################################### jQuery FAQ #############################################################
 
 //     Create some CSS rules that sets elements with the invisible class to visibility: hidden;.
@@ -14,27 +15,41 @@ $('#showHide').click(function (){
 $('dt').click(function (){
     $(this).css({
         'background-color': 'lightblue'
-    })
+    });
 });
+
 
 // ###################################### jQuery Traversal Exercise ###############################################
 
-//     Under the FAQ, add 3 unordered lists like above. Each list should contain a national park name in an h3 element,
-//     and a ul of 4 facts about the park.
-
-
-//     Create a button that, when clicked, makes the last li in each ul have a yellow background.
-//
-//     When any h3 is clicked, the lis underneath it should be bolded. Use font-weight: bold to achieve this.
-//
-//     When any list item is clicked, first li of the parent ul should have a font color of blue.
-//
-//     Bonus
+    
+// Create a button that, when clicked, makes the last li in each ul have a yellow background.
+    $('#last-li-highlighter').click(function (){
+        $('ul li:last-child').css({
+            'background-color': 'yellow'
+        });
+    });
+// 
+// When any h3 is clicked, the lis underneath it should be bolded. Use font-weight: bold to achieve this.
+    $('h3').click(function (){
+        $('ul').children().css({
+            'font-weight' : 'bold'
+        });
+    });
+// When any list item is clicked, first li of the parent ul should have a font color of blue.
+    $('li').click(function (){
+        $('ul li:first-child').css({
+            'color': 'blue'
+        })
+    })
+// 
+// Bonus
 // Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
-//
-//     The rules are the following:
-//
-//     The left frame swaps to the right and takes the image from the frame in the center.
-//     The center frame swaps randomly to either the left or right.
-//     The right frame swaps to the left and takes the image from the frame in the center.
+// 
+// The rules are the following:
+// 
+// The left frame swaps to the right and takes the image from the frame in the center.
+// The center frame swaps randomly to either the left or right.
+// The right frame swaps to the left and takes the image from the frame in the center.
+
+
 
