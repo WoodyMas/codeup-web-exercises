316c1510b09e9cf77a5c8dda930e986437aac945
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-22 10:55:32 -0500

    more examples of DOM traversal

--- js/jquery-dom-traversal.js
+++ js/jquery-dom-traversal.js
@@ -1,5 +1,40 @@
-$(function (){
-    $('li').each(function (){
-        $(this).css('font-style', 'italic');
-    })
-})
+$(function (){ // START IIFE
+
+
+    // $(function (){
+    //     $('li').each(function (index){
+    //         if (index %2 !== 0) {
+    //             $(this).css('font-style', 'italic');
+    //         }
+    //
+    //     });
+    // $('li').css('font-style', 'italic');
+    // })
+
+    // $('main ul li').first().css('font-size', 'larger');
+    //
+    // $('li a').last().css('text-decoration', 'underline dotted');
+
+    $('button').on('click', function (){
+       // $('main').children().css('font-family', 'sans-serif');
+       $('ul').last().children().children().css('text-decoration', 'underline dotted');
+    });
+
+    // $('h2').hover(function (){
+    //     $(this).next().slideToggle();
+    // });
+    $('h2').css({
+        'color': 'blue',
+        'text-decoration': 'underline',
+        'cursor' : 'pointer'
+    }).on('click', function (){
+        $(this).next().slideToggle();
+    });
+
+    $('header p').hover(function (){
+        $(this).parent().toggleClass('darkMode');
+    });
+
+
+
+}); // END IIFE
