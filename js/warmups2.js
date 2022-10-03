b6033bd1fa561db6a4d37e748bb96713b4368f5c
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-03 09:35:10 -0500

    practicing DOM manipulation in warmups2 file

--- js/warmups2.js
+++ js/warmups2.js
@@ -10,3 +10,28 @@ let forOfTestArray = ['papa', 'pizzeria', 1, 5, 19, 'LOL'];
 //     console.log(item);
 // }
 
+//######################################## DOM WARMUPS ################################################################
+
+//When user clicks the button that has the text 'change heading', the text of the h1 element changes to read "I pwn the DOM";
+
+$('button').on('click', function (){
+    $('h1').text('I pwn the DOM');
+});
+
+//// Every time the user enters a character in the input with id "source", that
+// // character appears in the <p> element with the id "output"
+
+// let inputSrc = document.getElementById('source');
+// let userText = inputSrc.val
+
+// formInput.onkeyup = function (){
+//     console.log(formInput.value);
+// }
+
+// document.getElementsByTagName('button')[0].addEventListener('click', function (){
+//     document.getElementsByTagName('h1')[0].innerText = "I still pwn the DOM w JS";
+// });
+
+// $('#source').on('keyup', function (){
+//     $('#output').text($('#source').val());
+// });
