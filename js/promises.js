02621987da87963fb24f79aebf83db0d57c5aef7
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-04 11:08:38 -0500

    beginning promise exercises

--- js/promises.js
+++ js/promises.js
@@ -1,17 +1,17 @@
 // AJAX request with jQuery
-$.get('data/murals.json', function (data){
-    console.log(data);
-});
+// $.get('data/murals.json', function (data){
+//     console.log(data);
+// });
 
 // AJAX request with Fetch API
-fetch('data/murals.json').then((response) =>{
-    return response.json();
-}).then((data) =>{
-    console.log(data)
-});
+// fetch('data/murals.json').then((response) =>{
+//     return response.json();
+// }).then((data) =>{
+//     console.log(data)
+// });
 
 // Abbreviated Fetch request using arrow functions
-fetch('data/murals.json').then(resp => resp.json()).then(data => console.log(data));
+// fetch('data/murals.json').then(resp => resp.json()).then(data => console.log(data));
 
 // Callback: a function passed as an argument to another function. It can run after another function has finished running
 //
@@ -36,31 +36,35 @@ fetch('data/murals.json').then(resp => resp.json()).then(data => console.log(dat
 //
 // sayHello('YoooOOOrr', 'Mac');
 
-function displayToOutputDiv (output) {
-    $('#output').html(output);
-}
+// function displayToOutputDiv (output) {
+//     $('#output').html(output);
+// }
+//
+// greeter('hola', 'Mason', displayToOutputDiv);
+//
+// function greeter(greeting, name, displayFunction){
+//     greeting = `${greeting}, ${name}!`;
+//     displayFunction(greeting);
+// }
+//
+// const aPromise = new Promise((resolve, reject) =>{
+//     setTimeout(()=>{
+//         (Math.random() > 0.5) ? resolve('Look Mah it worked!') : reject('Mah it din work...');
+//     }, 3000);
+// });
+//
+// aPromise.then(value => console.log(value)).catch(error=>console.log(error));
+//
+// const myFetchPromise = fetch('data/murals.json').then(response => {
+//     console.log(response.status);
+//     console.log(response.headers);
+//     console.log(response.url);
+//     return response.json();
+// }).then(data => console.log(data)).catch(error => console.log('It din werk'))
+//     .finally(()=>console.log('It will happen someday...'));
+//
+// console.log(myFetchPromise);
 
-greeter('hola', 'Mason', displayToOutputDiv);
 
-function greeter(greeting, name, displayFunction){
-    greeting = `${greeting}, ${name}!`;
-    displayFunction(greeting);
-}
+// ############################################### Exercise ##########################################################
 
-const aPromise = new Promise((resolve, reject) =>{
-    setTimeout(()=>{
-        (Math.random() > 0.5) ? resolve('Look Mah it worked!') : reject('Mah it din work...');
-    }, 3000);
-});
-
-aPromise.then(value => console.log(value)).catch(error=>console.log(error));
-
-const myFetchPromise = fetch('data/murals.json').then(response => {
-    console.log(response.status);
-    console.log(response.headers);
-    console.log(response.url);
-    return response.json();
-}).then(data => console.log(data)).catch(error => console.log('It din werk'))
-    .finally(()=>console.log('It will happen someday...'));
-
-console.log(myFetchPromise);
